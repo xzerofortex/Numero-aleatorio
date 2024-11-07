3b3b77fff1ac62e7204c3b72424fa5d65c07c698
zeroforte <147382689+xzerofortex@users.noreply.github.com>
2024-11-07 11:50:28 -0800

    Actualizar Adivina el número aleatorio.py

--- Adivina el número magico.py
+++ Adivina el número magico.py
@@ -1,24 +1,38 @@
-#adivina el numero magico  que piense el bot
+#adivina el numero  que escoja la variable bot
 import random
 
-# el bot piensa en un numero del 1 al 10
-bot =random.randint(1,10)
+minimo = 0
+
+dificultad = input("escoje una dificultad  (facil/medio/dificil/extremo):  ")
+if dificultad == 'facil':
+	maximo = 10
+elif dificultad == "medio":
+	maximo = 50
+elif dificultad == "dificil":
+	maximo = 100
+elif dificultad == "extremo":
+	maximo = 1000
+else :
+	print("escoje una dificultad:  ")
+	maximo = 10
+				
+bot = random.randint(minimo , maximo)
 intentos = 0
 
-# para que no termine de repetirse asta que ganes
 while True:
-    you =int(input('introduce un numero del 1 al 10:  '))
+    you =int(input(f'introduce un numero de ({minimo} - {maximo}):   '))
 
-    # valida si el numero que pusistes es del 1 al 10 sino hace que lo pongas de nuevo
-    if you not in range(1,11):
-        print('numero no valido es del (1 al 10) intente de nuevo: ')
+
+    if you not in range(minimo , maximo + 1):
+        print('numero no valido es de ({minimo} - {maximo}) intente de nuevo: ')
         continue
 
     intentos += 1  # se van acomulando los intentos asta que ganes
     if you == bot:
-        print(f'adivinastes el numero magico felizidades  numero:({you})  intentos:({intentos})')
+        print(f'adivinastes el numero felizidades  numero:({you})  intentos:(#{intentos})')
         break # finaliza el bucle, si ganas
     elif you < bot:
-        print(f'el numero magico es mayor a ({you})')
+        print(f'el numero es mayor a ({you}) intentos: (#{intentos}) ')
     elif you > bot:
-        print(f'el nuemro magico es menor  a  ({you})')
+        print(f'el nunero es menor a ({you}) intentos: (#{intentos}) ')
+       
